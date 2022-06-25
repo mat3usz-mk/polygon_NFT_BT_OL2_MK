d370aa19c08ecbe44bf6813dda52d616f1214e58
mat3usz-mk <matueszkozdras10@gmail.com>
2022-06-25 23:43:24 +0200

    second commit

--- contracts/NFT_MK.sol
+++ contracts/NFT_MK.sol
@@ -1,15 +1,15 @@
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+pragma solidity ^0.8.0; //My main goal is to build NFT on layer 2
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; //I used Open Zeppelin's ERC721 contracts, which reduced the amount of code to write.
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyNFT is ERC721, Ownable {
-  using Counters for Counters.Counter;
+  using Counters for Counters.Counter; //I defined some variables to help myself
   using Strings for uint256;
   Counters.Counter private _tokenIds;
   mapping (uint256 => string) private _tokenURIs;
   
   constructor() ERC721("MK_mn_FIRST_NFT", "MNFT") {}
-  function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+  function _setTokenURI(uint256 tokenId, string memory _tokenURI) //The function _setTokenURI is taking in a token ID and a token URI and adding it to the mapping.
     internal
     virtual
   {
@@ -26,7 +26,7 @@ contract MyNFT is ERC721, Ownable {
     string memory _tokenURI = _tokenURIs[tokenId];
     return _tokenURI;
   }
-  function mint(address recipient, string memory uri)
+  function mint(address recipient, string memory uri) //This function takes in a wallet address and a token URI. The token URI is used in the _setTokenURI function, the token ID is incremented. The new token is sent to the recipient's address.
     public
     returns (uint256)
   {
